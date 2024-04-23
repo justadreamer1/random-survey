858a9522ce4d04475934a0ae85013987c719023b
justadreamer1 <167875394+justadreamer1@users.noreply.github.com>
2024-04-23 20:02:44 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,12 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://docs.google.com/forms/d/e/1FAIpQLScBKSI33eNA7xfhpQEYdhGiXvMBuRp7vtPC74v91uEIJigvKQ/viewform?usp=sf_link',
+	'https://docs.google.com/forms/d/e/1FAIpQLSeXXs1hrxkl8nPjUYK0Sj9sEhhLxFJEP8oAPqcjXJ8rFALBYQ/viewform?usp=sf_link',
+	'https://docs.google.com/forms/d/e/1FAIpQLSckHDqZRFWOkgMMleCj1N2US701Ow-blqT-0YRZXIC8SZxhIA/viewform?usp=sf_link',
+	'https://docs.google.com/forms/d/e/1FAIpQLSdoGyZQuTykJh4b15gCA4JF-sQf3nxr0hCjriEmUB05OxXDvw/viewform?usp=sf_link',
+	'https://docs.google.com/forms/d/e/1FAIpQLScfujJDU99wG6DR_ZE813dj9TLDkwSEg1Nhy4VnzP1C-YecPw/viewform?usp=sf_link',
+	'https://docs.google.com/forms/d/e/1FAIpQLSd8v1G9pwONajzDZiNJ6pow2g1y02HHBrAdg-o_hjwVo4so4A/viewform?usp=sf_link',
+	'https://docs.google.com/forms/d/e/1FAIpQLSf7SoX8hhFCMUyjRnkPgNzTWzOTPlE8-9gtyMZ-O1lw7Tieww/viewform?usp=sf_link',
+	'https://docs.google.com/forms/d/e/1FAIpQLSeL92KzrAxpV-QC4-cQ608hAWw_vtjmTtMjnMWpNpIwtf7ZUg/viewform?usp=sf_link'
 ];
